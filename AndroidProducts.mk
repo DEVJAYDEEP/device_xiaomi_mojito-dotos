566c3625d0bb9063f0798186eaf30a0e24dde773
Khuong Hung <46420538+khuonghung1423@users.noreply.github.com>
2021-09-01 11:56:45 +0700

    mojito: CherishOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The PixelExperience Project
+# Copyright (C) 2021 CherishOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mojito.mk
+    $(LOCAL_DIR)/cherish_mojito.mk
+
+COMMON_LUNCH_CHOICES := \
+    cherish_mojito-user \
+    cherish_mojito-userdebug \
+    cherish_mojito-eng
--- cherish_mojito.mk
+++ cherish_mojito.mk
@@ -7,18 +7,27 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/xiaomi/ANXCamera/config.mk)
 
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common PixelExperience stuff
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common CherishOS stuff
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+
+CHERISH_BUILD_TYPE := OFFICIAL
+WITH_GMS := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_INCLUDE_STOCK_ARCORE := false
+
+# Maintainer
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer=KhuongHung|WaIIeeD
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_mojito
+PRODUCT_NAME := cherish_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
